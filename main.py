eb5314d85b2e59ef110db58d862d9d0e4ae43a44
Harry Nash <harrywilliamnash@gmail.com>
2020-04-28 20:30:24 +0100

    Use new discord syntax

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import discord, asyncio
-
-from secret import discord_token
 from commands import hands, jumble, solve, custom, text
 from cube_constants import (HANDS_OP, JUMBLE_OP, SOLVE_OP, CUSTOM_OP, TEXT_OP,
 	HELP_OP, GRAFFITIED_IMAGE, HELP_TEXT)
@@ -8,39 +6,38 @@ from cube_constants import (HANDS_OP, JUMBLE_OP, SOLVE_OP, CUSTOM_OP, TEXT_OP,
 client = discord.Client()
 lock = asyncio.Lock()
 
-@client.event
-async def on_ready():
-	"""Indicates the bot is ready by printing it's credentials."""
-	print('Logged in as')
-	print(client.user.name)
-	print(client.user.id)
-	print('------')
+class MyClient(discord.Client):
 
-@client.event
-async def on_message(message):
-	"""Detects and carries out any commands then sends a cube image or text."""
-	txt = None
-	await lock.acquire()
-	if (message.content.startswith(HANDS_OP)):
-		txt = hands(message.channel.id, message.content[len(HANDS_OP + ' '):])
-	elif (message.content.startswith(JUMBLE_OP)):
-		jumble(message.channel.id)
-	elif (message.content.startswith(SOLVE_OP)):
-		solve(message.channel.id)
-	elif (message.content.startswith(CUSTOM_OP)):
-		txt = custom(message.channel.id, message.content[len(CUSTOM_OP + ' '):])
-	elif (message.content.startswith(TEXT_OP)):
-		txt = text(message.channel.id)
-	elif (message.content.startswith(HELP_OP)):
-		txt = HELP_TEXT
-	else:
+    async def on_ready(self):
+        print('Logged on as', self.user)
+
+	async def on_message(self, message):
+		"""Detects and carries out any commands then sends a cube image or text."""
+		txt = None
+		await lock.acquire()
+		if (message.content.startswith(HANDS_OP)):
+			txt = hands(message.channel.id, message.content[len(HANDS_OP + ' '):])
+		elif (message.content.startswith(JUMBLE_OP)):
+			jumble(message.channel.id)
+		elif (message.content.startswith(SOLVE_OP)):
+			solve(message.channel.id)
+		elif (message.content.startswith(CUSTOM_OP)):
+			txt = custom(message.channel.id, message.content[len(CUSTOM_OP + ' '):])
+		elif (message.content.startswith(TEXT_OP)):
+			txt = text(message.channel.id)
+		elif (message.content.startswith(HELP_OP)):
+			txt = HELP_TEXT
+		else:
+			lock.release()
+			return
+
+		if (txt == None):
+			await message.channel.send_file("cube.png", filename=GRAFFITIED_IMAGE, content="cube")
+		else:
+			await message.channel.send(txt)
 		lock.release()
-		return
 
-	if (txt == None):
-		await client.send_file(message.channel, GRAFFITIED_IMAGE)
-	else:
-		await client.send_message(message.channel, txt)
-	lock.release()
+if __name__ == "__main__":
+	client = MyClient()
 
-client.run(discord_token)
+	client.run(mytoken)
